00c39b68273a6cab212cb1b6cf5348087499754d
Thuc Nguyen <thucnguyen61098@gmail.com>
2025-08-11 18:58:16 -0400

    Added argparsing

--- updateSongTitle.py
+++ updateSongTitle.py
@@ -1,17 +1,24 @@
 import taglib
+import argparse
 import os
-# TODO: ADD THIS TO A GIT REPO
-# TODO: add a help runtime option with usage information
 
-pathToMusic = input("Path to Music Files: ") # path in linux format in WSL terminal if on windows (ex. /mnt/c/Users/Thuc/Music/New/test)
+# Parse command line args
+parser = argparse.ArgumentParser()
+parser.add_argument("-h", "--help", dest = "help", default = "", help="Print Usage Information")
+parser.add_argument("-s", "--source", dest = "source", default = "/", help="Source File Path")
+parser.add_argument("-d", "--dest", dest = "dest", default = "/", help="Destination File Path")
+parser.add_argument("-v", "--verbose", dest = "verbose", default = "", help="Verbose Output")
+args = parser.parse_args()
 
-# fileList = os.listdir(pathToMusic)
+# Get source path to music from arguments
+#pathToMusic = input("Path to Music Files: ") # path in linux format in WSL terminal if on windows (ex. /mnt/c/Users/Thuc/Music/New/test)
+pathToMusic = args.source
 
+# Print files under source path
 dirScan = os.scandir(pathToMusic)
-
 print("Files under ", pathToMusic, ": ")
-# print(fileList)
 
+# Update names of songs under source path
 for musicFile in dirScan:
     if musicFile.is_file():
         print(musicFile.name)
