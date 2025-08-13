d16c25e2238b50e1c0b2bb3175c1384b30e7d195
Thuc Nguyen <thucnguyen61098@gmail.com>
2025-08-12 23:46:11 -0400

    Updated some of the argument declarations

--- updateSongTitle.py
+++ updateSongTitle.py
@@ -4,10 +4,9 @@ import os
 
 # Parse command line args
 parser = argparse.ArgumentParser()
-parser.add_argument("-h", "--help", dest = "help", default = "", help="Print Usage Information")
-parser.add_argument("-s", "--source", dest = "source", default = "/", help="Source File Path")
-parser.add_argument("-d", "--dest", dest = "dest", default = "/", help="Destination File Path")
-parser.add_argument("-v", "--verbose", dest = "verbose", default = "", help="Verbose Output")
+parser.add_argument("-s", "--source", dest = "source", required=True, default = "/", help="Source File Path")
+parser.add_argument("-d", "--dest", dest = "dest", required=True, default = "/", help="Destination File Path")
+parser.add_argument("-v", "--verbose", action='store_true', help="Verbose Output")
 args = parser.parse_args()
 
 # Get source path to music from arguments
@@ -15,8 +14,9 @@ args = parser.parse_args()
 pathToMusic = args.source
 
 # Print files under source path
-dirScan = os.scandir(pathToMusic)
-print("Files under ", pathToMusic, ": ")
+if args.verbose:
+    dirScan = os.scandir(pathToMusic)
+    print("Files under ", pathToMusic, ": ")
 
 # Update names of songs under source path
 for musicFile in dirScan:
@@ -32,10 +32,10 @@ for musicFile in dirScan:
         #song.save()
         #print("New song TITLE: ", songTags['TITLE'], " and artist: ", songTags['ARTIST'])
 
-        # TODO: turn into an option at runtime instead of just commenting it in and out when I need it
-        if len(songTags) > 0:
-            max_key_len = max(len(key) for key in songTags.keys())
-            for key, values in songTags.items():
-                for value in values:
-                    print(f"  {key.ljust(max_key_len)} = {value}")
+        if args.verbose:
+            if len(songTags) > 0:
+                max_key_len = max(len(key) for key in songTags.keys())
+                for key, values in songTags.items():
+                    for value in values:
+                        print(f"  {key.ljust(max_key_len)} = {value}")
             
