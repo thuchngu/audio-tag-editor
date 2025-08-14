0b24f9de412d728943f923a368f2a16461690c4b
Thuc Nguyen <thucnguyen61098@gmail.com>
2025-08-13 20:33:04 -0400

    Fixed args and added arg for updating tags

--- updateSongTitle.py
+++ updateSongTitle.py
@@ -5,18 +5,19 @@ import os
 # Parse command line args
 parser = argparse.ArgumentParser()
 parser.add_argument("-s", "--source", dest = "source", required=True, default = "/", help="Source File Path")
-parser.add_argument("-d", "--dest", dest = "dest", required=True, default = "/", help="Destination File Path")
 parser.add_argument("-v", "--verbose", action='store_true', help="Verbose Output")
+parser.add_argument("-u", "--update", action='store_true', help="Update the tags in source")
 args = parser.parse_args()
 
 # Get source path to music from arguments
 #pathToMusic = input("Path to Music Files: ") # path in linux format in WSL terminal if on windows (ex. /mnt/c/Users/Thuc/Music/New/test)
 pathToMusic = args.source
+showVerbose = args.verbose
+doUpdate = args.update
 
 # Print files under source path
-if args.verbose:
-    dirScan = os.scandir(pathToMusic)
-    print("Files under ", pathToMusic, ": ")
+dirScan = os.scandir(pathToMusic)
+print("Files under ", pathToMusic, ": ")
 
 # Update names of songs under source path
 for musicFile in dirScan:
@@ -25,14 +26,15 @@ for musicFile in dirScan:
         song = taglib.File(musicFile)
         songTags = song.tags
 
-        # TODO: turn into an option at runtime instead of just commenting it in and out when I need it
-        #songTitle = musicFile.name
-        #print("Current song title: ", songTags['TITLE'], " and artist: ", songTags['ARTIST'])
-        #songTags['TITLE'] = [musicFile.name[3:].replace(".wav", "")]
-        #song.save()
-        #print("New song TITLE: ", songTags['TITLE'], " and artist: ", songTags['ARTIST'])
+        # TODO Add Album and Artist update here as well
+        if doUpdate:
+            songTitle = musicFile.name
+            print("Updating song title: ", songTags['TITLE'], " and artist: ", songTags['ARTIST'], " to ", songTitle)
+            # TODO check for different file extensions other than just wav
+            songTags['TITLE'] = [musicFile.name[3:].replace(".wav", "")]
+            song.save()
 
-        if args.verbose:
+        if showVerbose:
             if len(songTags) > 0:
                 max_key_len = max(len(key) for key in songTags.keys())
                 for key, values in songTags.items():
